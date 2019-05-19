8037b93a49824802f11453e2f0388bcb0c805454
Patricio <wearetamo@gmail.com>
2019-05-18 22:29:29 -0400

    searching using props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,8 @@ class SearchBar extends React.Component {
 
     onFormSubmit= event => {
         event.preventDefault();
-        
+        this.props.onFormSubmit(this.state.term)
+
     }
 
     render() {
