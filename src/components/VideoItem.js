7ac0f60f4cc94f4b7d354ac5105cb67f7f56008e
Patricio <wearetamo@gmail.com>
2019-05-18 23:03:17 -0400

    showing images from videos

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const VideoItem = props => {
-    return <div>Video Item</div>
+const VideoItem = ({ video }) => {
+    console.log(video);
+    return (
+        <div>
+            <img src={video.snippet.thumbnails.medium.url} alt="youtube video"/>
+            {video.snippet.title}
+        </div>
+    )
+  
 }
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({videos}) => { //Functionla Component
 
     const RENDERED_LIST = videos.map(video=>{
-        return <VideoItem/>
+        return <VideoItem video={video}/>
     })
     return <div>{RENDERED_LIST}</div>
 }
