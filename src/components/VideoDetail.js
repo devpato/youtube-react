955fbb814e45b6b559e8e5b0aec0759b8340576b
Patricio <wearetamo@gmail.com>
2019-05-19 00:04:17 -0400

    displaying video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import './VideoDetails.css'
 const VideoDetail = ({video}) => {
-    if(video) {    
+    if(video) {  
+        const YOUTUBE_VIDEO = `https://www.youtube.com/embed/${video.id.videoId}`;  
         return (
-            <div className="ui segment">
-                <h4 className="ui header">{video.snippet.title}</h4>
-                <p>
-                    {video.snippet.description}
-                </p>
+            <div>
+                <div className="ui embeded">
+                    <iframe className="video-player" src={YOUTUBE_VIDEO}/>
+                </div>
+                <div className="ui segment">
+                    <h4 className="ui header">{video.snippet.title}</h4>
+                    <p>
+                        {video.snippet.description}
+                    </p>
+                </div>
             </div>
         )
     }
