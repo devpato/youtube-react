97c93e18f84ec9c5e20bbed36851d35d1380afa3
Patricio <wearetamo@gmail.com>
2019-05-19 00:13:49 -0400

    fixed key warning

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 const VideoList = ({videos, onVideoSelect}) => { //Functionla Component
-
-    const RENDERED_LIST = videos.map(video=>{
-        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
+    const RENDERED_LIST = videos.map(video =>{
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>
     });
     
     return <div className="ui relaxed divided list">{RENDERED_LIST}</div>
