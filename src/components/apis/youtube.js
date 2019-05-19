14e68368c90d7fe6d65cf8ffff7e4ce3bf7cee34
Patricio <wearetamo@gmail.com>
2019-05-18 22:15:57 -0400

    using axios for api call

---
+++ src/components/apis/youtube.js
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+const KEY = 'AIzaSyCHITA0aj4hs02BhwJLS6eQ68GnZ2VDK_U';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
